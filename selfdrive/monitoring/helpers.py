955c722dca217c70882dd44ae05887cd0ac23930
charshuck9s <char.shuck.9s@icloud.com>
2025-01-22 13:56:39 -0800

    Update helpers.py

--- selfdrive/monitoring/helpers.py
+++ selfdrive/monitoring/helpers.py
@@ -21,9 +21,9 @@ class DRIVER_MONITOR_SETTINGS:
   def __init__(self):
     self._DT_DMON = DT_DMON
     # ref (page15-16): https://eur-lex.europa.eu/legal-content/EN/TXT/PDF/?uri=CELEX:42018X1947&rid=2
-    self._AWARENESS_TIME = 60. # passive wheeltouch total timeout
-    self._AWARENESS_PRE_TIME_TILL_TERMINAL = 30.
-    self._AWARENESS_PROMPT_TIME_TILL_TERMINAL = 15.
+    self._AWARENESS_TIME = 300. # passive wheeltouch total timeout
+    self._AWARENESS_PRE_TIME_TILL_TERMINAL = 150.
+    self._AWARENESS_PROMPT_TIME_TILL_TERMINAL = 60.
     self._DISTRACTED_TIME = 240. # active monitoring total timeout
     self._DISTRACTED_PRE_TIME_TILL_TERMINAL = 180.
     self._DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 120.
